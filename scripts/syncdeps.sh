8f09a3bd23ad3107e81bf82d257a1123592a0c24
Elliot Murphy <statik@users.noreply.github.com>
2019-10-11 17:20:54 -0400

    i don't know bash
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- scripts/syncdeps.sh
+++ scripts/syncdeps.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 export RUNFILES_DIR="$PWD/.."
 export PATH="$PWD/external/go_sdk/bin:$PATH"
-local gazelle="$PWD/$1"
+gazelle="$PWD/$1"
 
 echo "Using these commands"
 command -v go
