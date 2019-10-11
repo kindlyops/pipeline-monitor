a157620e1f1bd2c5c76d9b50e3cc5c2fa1991337
Elliot Murphy <statik@users.noreply.github.com>
2019-10-11 18:36:56 -0400

    better debug
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- scripts/commit-syncdeps.sh
+++ scripts/commit-syncdeps.sh
@@ -5,6 +5,16 @@ set -euo pipefail
 git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
 git config --global user.name "$GITHUB_ACTOR"
 
+cat <<- EOF > "$HOME/.netrc"
+machine github.com
+login $GITHUB_ACTOR"
+password $GITHUB_TOKEN
+machine api.github.com
+login $GITHUB_ACTOR
+password $GITHUB_TOKEN
+EOF
+chmod 600 "$HOME/.netrc"
+
 git add vendor
 
 if ! git diff --quiet
@@ -12,20 +22,10 @@ then
     echo "Committing changes from syncdeps"
     # shellcheck disable=SC2046
     PUSH_BRANCH=$(echo "$GITHUB_REF" | awk -F / '{ print $3 }')
+    echo "Push branch is $PUSH_BRANCH"
     git checkout "$PUSH_BRANCH"
     git add WORKSPACE
     git commit -m "Committing changes from syncdeps (go mod vendor && gazelle)"
-
-    cat <<- EOF > "$HOME/.netrc"
-    machine github.com
-    login $GITHUB_ACTOR"
-    password $GITHUB_TOKEN
-    machine api.github.com
-    login $GITHUB_ACTOR
-    password $GITHUB_TOKEN
-EOF
-    chmod 600 "$HOME/.netrc"
-
     git push origin "$PUSH_BRANCH"
 else
     echo "No changes found to commit."
