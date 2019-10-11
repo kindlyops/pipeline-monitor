45afdedc320488f348f28e097da43c917d02f687
Elliot Murphy <statik@users.noreply.github.com>
2019-10-11 16:40:19 -0400

    linters and syncdeps
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

---
+++ scripts/syncdeps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefile
+
+export RUNFILES_DIR="$PWD/.."
+export PATH="$PWD/external/go_sdk/bin:$PATH"
+local gazelle="$PWD/$1"
+
+echo "Using these commands"
+command -v go
+echo "$gazelle"
+
+
+go tidy
+go mod vendor
+$gazelle update-repos -from_file=go.mod
